67738aabcacb56cd90f67bd94d8f0d9ab94514da
Sergey Dmitriev <serge.dmitriev@gmail.com>
2015-01-20 14:49:26 +0700

    separate app & routes

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 "use strict";
 
 var express = require('express'),
-    app = express(),
+	bodyParser = require('body-parser'),
   	swig = require('swig'),
-    routes = require('./routes/index'),
-  	path = require('path');
-	
+  	path = require('path'),
+  	routes = require('./routes/index');
+
+var app = express();
   
 app.set('port', process.env.PORT || 3000);
-
-app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
-//app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.engine('html', swig.renderFile);
+
+app.use(bodyParser());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 app.listen(app.get('port'), function(){
--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,15 @@ var MongoClient = require('mongodb').MongoClient,
 var express = require('express');
 var router = express.Router();
 
-MongoClient.connect('mongodb://localhost:27017/wiki', function(err, dbconn) {
+
+MongoClient.connect('mongodb://localhost:27017/wiki', function (err, dbconn) {
   db = dbconn;
 });
 
 
 router.get('/areas', function(req, res) {
   var areasArray = [];
-  db.collection('mems').find({}, {'areas': true, '_id': false}).toArray(function(err, areas) {  
+  db.collection('mems').find({}, {'areas': true, '_id': false}).toArray(function (err, areas) {  
     for (var i = 0, l = areas.length; i < l; i++) {
       areasArray.push(areas[i].areas);
     }
@@ -21,42 +22,49 @@ router.get('/areas', function(req, res) {
   });
 });
 
+
 router.get('/mems', function(req, res) {
   db.collection('mems').find().sort({name: 1}).toArray(function(err, mems) {
     res.json(mems);
   });
 });
 
+
 router.post('/mems', function(req, res) {
   if (req.body._id) { 
-  req.body._id = new ObjectID(req.body._id);
-    db.collection('mems').update({'_id': req.body._id}, req.body, {'upsert': true}, function(err, data) {
-    if (err) throw err;
-	db.collection('mems').findOne({_id: req.body._id}, function(err, data) {
-	  if (err) throw err;
-	  res.json(data);
-	});
-  });
-} else {	
-  db.collection('mems').insert(req.body, function(err, data) {
-    if (err) throw err;
-    res.json(data[0]);
-  });
-}
+    req.body._id = new ObjectID(req.body._id);
+    db.collection('mems')
+      .update(
+        {'_id': req.body._id},
+        req.body, 
+        {'upsert': true}, 
+        function(err, data) {
+          if (err) throw err;
+          db.collection('mems')
+            .findOne({_id: req.body._id}, function(err, data) {
+          	  if (err) throw err;
+          	  res.json(data);
+          	});
+        });
+  } else {	
+    db.collection('mems').insert(req.body, function(err, data) {
+      if (err) throw err;
+      res.json(data[0]);
+    });
+  }
 });
+
 
 router.get('/delete/:id', function(req, res) {   
-  var objID;
+  var objID = new ObjectID(req.params.id); 
 
-  try { 
-    objID = new ObjectID(req.params.id); 
-  } catch (e) {}
-
-  db.collection('mems').remove({_id: objID},function(err,data){ res.json(data);});
+  db.collection('mems').remove({_id: objID}, function (err,data) { res.json(data);});
 });
+
   
 router.get('/', function(req, res) {
     res.render('home');
 });
+
 
 module.exports = router;
